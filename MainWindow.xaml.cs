f04d47ac3819107454241502939da4a22ca58b64
184517 <47399915+184517@users.noreply.github.com>
2019-06-06 14:20:19 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿/* Jordan Ross
+ * June 5, 2019
+ * Sum of the squares from one to one hundred minus the square of the sum from one to one hunred
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Euler_SumSquareDifference
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double SumOfSquares;
+        double SquareOfSum = 0;
+        double finalSum;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            double result = 0;
+            double sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                SumOfSquares += i * i;
+                SquareOfSum += i;
+            }
+
+            result = SquareOfSum;
+            sum = result * result;
+            finalSum = sum -SumOfSquares;
+            MessageBox.Show(finalSum.ToString());
+
+
+
+        }
+    }
+}
